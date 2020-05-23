0c9dda523333e202b613aa039479629665b496e7
tnrwl4 <tnrwl4@naver.com>
2020-05-24 02:38:50 +0900

    유니티 튜토리얼 Part 4 - 컬라이더 1. 컬라이더 사용 2. RayCast 사용 3. OnTriggerEnter, OnTriggerStay, OnTriggerExit 사용법

---
+++ 유니티 튜토리얼/Assets/Script/Part4Test.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part4Test : MonoBehaviour
+{
+    private BoxCollider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = this.GetComponent<BoxCollider>();
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    //if(Input.GetKeyDown(KeyCode.W))
+    //    //{
+    //    //    Debug.Log("col.bounds : " + col.bounds);
+    //    //    Debug.Log("col.bounds.extents : " + col.bounds.extents);
+    //    //    Debug.Log("col.bounds.extents.x : " + col.bounds.extents.x);
+    //    //    Debug.Log("col.size : " + col.size);
+    //    //    Debug.Log("col.center : " + col.center);
+    //    //}
+
+    //    //col.size = new Vector3(3, 3, 3);
+
+    //    //if(Input.GetMouseButtonDown(0))
+    //    //{
+    //    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    //    RaycastHit hitinfo;
+    //    //    if(col.Raycast(ray, out hitinfo, 1000))
+    //    //    {
+    //    //        this.transform.position = hitinfo.point;
+    //    //    }
+    //    //}
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.position += new Vector3(0, 2, 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+    }
+}
