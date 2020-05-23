83fd275fdfc8d1f09599644d24aa7c3c7299f809
tnrwl4 <tnrwl4@naver.com>
2020-05-24 02:07:45 +0900

    유니티 입문강좌 Part 3 - 리지드 바디 완료 1. 물리효과 적용 방법 2. 오브젝트 이동 속도 설정 3. 오브젝트 회전 속도와 최대 회전 속도 설정 4. AddForce와 MovePosition의 차이 5. AddTorque와 MoveRotation의 차이 6. AddExplosionForce 사용 방법

---
+++ 유니티 튜토리얼/Assets/Script/Part3Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+public class Part3Test : MonoBehaviour
+{
+    private Rigidbody myRigid;
+    private Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = this.GetComponent<Rigidbody>();
+        rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //myRigid.velocity = new Vector3(0, 0, 1);
+            //myRigid.angularVelocity = new Vector3(0, 0, 1);
+            //myRigid.maxAngularVelocity = 100;
+            //myRigid.angularVelocity = Vector3.right * 100;
+            //myRigid.MovePosition(this.transform.forward * Time.deltaTime);
+            //rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            //myRigid.MoveRotation(Quaternion.Euler(rotation));
+            //myRigid.AddForce(Vector3.forward);
+            //myRigid.AddTorque(Vector3.up);
+            myRigid.AddExplosionForce(10, transform.right, 10);
+        }
+    }
+}
