ec250eda19e50e7bb1fe978e22a9ad82e648fce5
tnrwl4 <tnrwl4@naver.com>
2020-05-24 03:26:34 +0900

    유니티 입문 강좌 Part 5 - 메쉬 렌더러 완료 1. Material 종류 변경 2. Material 색상 변경 3. Material에 Light 적용

---
+++ 유니티 튜토리얼/Assets/Script/Part5Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Part5Test : MonoBehaviour
+{
+    [SerializeField]
+    private Material red_mat;
+    [SerializeField]
+    private Material green_mat;
+
+    private MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mesh.material = green_mat;
+        }
+        else
+        {
+            mesh.material = red_mat;
+        }
+    }
+}
