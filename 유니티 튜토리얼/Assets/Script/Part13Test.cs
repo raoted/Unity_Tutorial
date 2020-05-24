b0370243ddd14e4246dd71b8121def93f070cb7d
tnrwl4 <tnrwl4@naver.com>
2020-05-24 23:14:46 +0900

    유니티 입문 강좌 Part 13 - 인풋 필드 완료 1. 인풋 필드 속성 확인 2. Script를 이용하여 인풋 필드 제어 3. 인풋 필드을 응용하여 입출금 시스템 구현 (출금시 예외처리 설정)

---
+++ 유니티 튜토리얼/Assets/Script/Part13Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part13Test : MonoBehaviour
+{
+    [SerializeField] private Text txt_Money;
+    [SerializeField] private InputField inputTxt_Money;
+
+    private int currentMoney = 0;
+
+    public void Input()
+    {
+        currentMoney += int.Parse(inputTxt_Money.text);
+
+        txt_Money.text = currentMoney.ToString();
+    }
+
+    public void Output()
+    {
+        if (currentMoney >= int.Parse(inputTxt_Money.text))
+        {
+            currentMoney -= int.Parse(inputTxt_Money.text);
+        }
+
+        txt_Money.text = currentMoney.ToString();
+    }
+}
