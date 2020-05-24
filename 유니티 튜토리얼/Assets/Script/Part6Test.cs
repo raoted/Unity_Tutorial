da8f105ed77b7b678006b6d6f8625784f4d0ceec
tnrwl4 <tnrwl4@naver.com>
2020-05-24 15:30:01 +0900

    유니티 입문 강좌 Part 6 - 카메라 완료 1. 카메라의 속성 2. 카메라 응용 3. 스크립트를 이용한 카메라 제어

---
+++ 유니티 튜토리얼/Assets/Script/Part6Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Part6Test : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go_target;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector3 difValue;
+    private Camera theCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difValue = this.transform.position - go_target.transform.position;
+        difValue = new Vector3(Mathf.Abs(difValue.x), Mathf.Abs(difValue.y), Mathf.Abs(difValue.z));
+        //theCam.fieldOfView = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_target.transform.position + difValue, speed);
+    }
+}
