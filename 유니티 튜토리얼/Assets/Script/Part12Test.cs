eaa612babd93dafeefc186b390fd7eb4306497bf
tnrwl4 <tnrwl4@naver.com>
2020-05-24 22:58:45 +0900

    유니티 입문 강좌 Part 12 - UI슬라이더 완료 1. UI 슬라이더의 속성 확인 2. Script를 이용하여 UI 슬라이더를 제어 3. UI 슬라이더를 응용하여 HP 구현

---
+++ 유니티 튜토리얼/Assets/Script/Part12Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part12Test : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    private bool isClick = false;
+
+    private float dotTime = 1f;
+    private float currentDotTime;
+    private void Start()
+    {
+        currentDotTime = dotTime;
+    }
+    private void Update()
+    {
+        if (isClick)
+        {
+            currentDotTime -= Time.deltaTime;
+
+            if(currentDotTime <= 0)
+            {
+                slider.value -= Time.deltaTime;
+
+                if(currentDotTime <= -1f)
+                {
+                    currentDotTime = dotTime;
+                }
+            }
+        }
+    }
+    public void Button()
+    {
+        isClick = true;
+    }
+}
