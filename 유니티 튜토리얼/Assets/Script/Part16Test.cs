62928eda508e28229e0cd73bc2035afb6e56977a
tnrwl4 <tnrwl4@naver.com>
2020-05-25 00:35:16 +0900

    유니티 입문 강좌 Part 16 - 오디오 소스 완료 1. 오디오 소스 속성 확인 2. Script를 이용하여 오디오 소스 제어 3. Script를 이용하여 랜덤한 오디오 재생

---
+++ 유니티 튜토리얼/Assets/Script/Part16Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part16Test : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();    
+    }
+
+    // Update is called once per frame
+    public void playSE()
+    {
+        int temp = Random.Range(0, 3);
+
+        theAudio.clip = clip[temp];
+        theAudio.Play();
+    }
+}
