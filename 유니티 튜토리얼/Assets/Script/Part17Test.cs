0dc2a6729e50d1490615f245dd9864e1c58992a8
tnrwl4 <tnrwl4@naver.com>
2020-05-25 01:24:55 +0900

    유니티 입문 강좌 Part 17 Physics 완료 1. Raycast를 사용했을 때 Raycast에 걸리지 않도록 설정하는 방법 2. collider 충동 무시하게 하기 3. Script를 이용하여 enemy와 player, bullet(prefab)을 제어하기

---
+++ 유니티 튜토리얼/Assets/Script/Part17Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part17Test : MonoBehaviour
+{
+    private float createTime = 1f;
+    private float currentCreateTime;
+
+    [SerializeField] private GameObject go_bullet;
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] col = Physics.OverlapSphere(transform.position, 5f);
+        if(col.Length > 0)
+        {
+            for(int i = 0; i < col.Length; i++)
+            {
+                Transform tf_target = col[i].transform;
+                if(tf_target.tag == "player")
+                {
+                    Quaternion rotation = Quaternion.LookRotation(tf_target.position - this.transform.position);
+                    transform.rotation = rotation;
+
+                    currentCreateTime += Time.deltaTime;
+
+                    if (currentCreateTime >= createTime)
+                    {
+                        Instantiate(go_bullet, transform.position, rotation);
+                        currentCreateTime = 0;
+                    }
+                }
+            }
+        }
+    }
+}
