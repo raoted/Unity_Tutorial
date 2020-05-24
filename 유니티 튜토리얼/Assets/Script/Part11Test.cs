4df99573fd1e788f629007d15f85c2f0deeee09b
tnrwl4 <tnrwl4@naver.com>
2020-05-24 21:55:24 +0900

    유니티 입문 강좌 Part11 - UI의 기본 완료 1. UI 사용 방법 2. Canvas의 속성 3. Image와 Text 사용 방법 4. Script를 이용한 UI 속성 제어

---
+++ 유니티 튜토리얼/Assets/Script/Part11Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part11Test : MonoBehaviour
+{
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image img_name;
+    [SerializeField] private Sprite sprite;
+
+    private bool isCoolTime = false;
+
+    private float currentTime = 5f;
+    private float delayTime = 5f;
+
+    void Update()
+    {
+        //Color color = img_name.color;
+        //color.a = 0;
+        //img_name.color = color;
+
+        if(isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime / delayTime;
+
+            if(currentTime <= 0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
+
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        //img_name.fillAmount = 0.5f;
+        isCoolTime = true;
+    }
+}
