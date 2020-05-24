865bd1628bf4b9758908da39d36c6ae99d0e496d
tnrwl4 <tnrwl4@naver.com>
2020-05-24 21:26:40 +0900

    유니티 입문 강좌 Part 10 - 파티클 시스템 완료 1. 파티클 시스템 속성 2. 스크립트를 이용하여 파티클 시스템 제어

---
+++ 유니티 튜토리얼/Assets/Script/Part10Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part10Test : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ps.Play();        //파티클 시스템 시작
+        //ps.Stop();        //파티클 시스템 중지
+        //ps.Pause();       //파티클 시스템 정지
+        //ps.Emit(n);         //파티클 n개 출력
+
+    }
+}
